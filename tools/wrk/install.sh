75d1256bcec1c8949154f4633953f7c781064d26
rutu-sh <ruturajs10@hotmail.com>
2024-10-19 18:19:02 -0400

    fix: fixed bash script

--- tools/wrk/install.sh
+++ tools/wrk/install.sh
@@ -24,22 +24,24 @@ function benchmark() {
 
     echo '"Time (s)","CPU (%)","RAM (kB)"'
     while true; do
-    etimes=$(ps -p "$pid" --no-headers -o etimes | awk '{ print $1 }')
-    pids=()
-    { exec >"$cpuf"; top -b -n 2 -d "$sint" -p "$pid" | {
-        grep "$pid" || echo; } | tail -1 | awk '{print (NF>0 ? $9 : "0")}'; } &
-    pids+=($!)
-    { exec >"$memf"; smem -H -U "$USER" -c 'pid pss' -P 'k6 run' | {
-        grep "$pid" || echo 0; } | awk '{ print $NF }'; } &
-    # pids+=($!)
-    # { exec >"$vusf"; { curl -fsSL http://localhost:6565/v1/metrics/vus 2>/dev/null || echo '{}'
-    #     } | jq '.data.attributes.sample.value // 0'; } &
-    # pids+=($!)
-    # { exec >"$rpsf"; { curl -fsSL http://localhost:6565/v1/metrics/http_reqs 2>/dev/null || echo '{}'
-    #     } | jq '.data.attributes.sample.rate // 0'; } &
-    # pids+=($!)
-    wait "${pids[@]}"
-    echo "${etimes},$(cat "$cpuf"),$(cat "$memf")"
+        etimes=$(ps -p "$pid" --no-headers -o etimes | awk '{ print $1 }')
+        pids=()
+        { exec >"$cpuf"; top -b -n 2 -d "$sint" -p "$pid" | {
+            grep "$pid" || echo; } | tail -1 | awk '{print (NF>0 ? $9 : "0")}'; } &
+        pids+=($!)
+        { exec >"$memf"; smem -H -U "$USER" -c 'pid pss' -P 'k6 run' | {
+            grep "$pid" || echo 0; } | awk '{ print $NF }'; } &
+        # pids+=($!)
+        # { exec >"$vusf"; { curl -fsSL http://localhost:6565/v1/metrics/vus 2>/dev/null || echo '{}'
+        #     } | jq '.data.attributes.sample.value // 0'; } &
+        # pids+=($!)
+        # { exec >"$rpsf"; { curl -fsSL http://localhost:6565/v1/metrics/http_reqs 2>/dev/null || echo '{}'
+        #     } | jq '.data.attributes.sample.rate // 0'; } &
+        # pids+=($!)
+        wait "${pids[@]}"
+        echo "${etimes},$(cat "$cpuf"),$(cat "$memf")"
     done
 
 }
+
+"$@"
