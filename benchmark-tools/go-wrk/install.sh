6c18729412f2fa8d0d2993acee135a81c52139a6
rutu-sh <ruturajs10@hotmail.com>
2024-10-20 10:55:07 -0400

    fix: fix setup scripts

--- benchmark-tools/go-wrk/install.sh
+++ benchmark-tools/go-wrk/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function install {
+    sudo apt-get install -y wrk smem jq
     rm -rf /tmp/go-wrk && git clone https://github.com/tsliwowicz/go-wrk.git /tmp/go-wrk && cd /tmp/go-wrk && go build && sudo mv go-wrk /usr/local/bin/
 }
 
