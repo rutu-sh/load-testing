a683bdfd8ee85a07ce491bc66924b608b17acbaa
rutu-sh <ruturajs10@hotmail.com>
2024-10-20 11:44:50 -0400

    fix: update script.js

--- benchmark-tools/k6/script.js
+++ benchmark-tools/k6/script.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const url = process.env.URL || 'https://test.k6.io';
+const url = __ENV.URL || 'https://test.k6.io';
 
 export const options = {
 };
