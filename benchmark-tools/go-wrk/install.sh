2cccbcb190ca285618dc7d35409990a0c4553fe5
rutu-sh <ruturajs10@hotmail.com>
2024-10-21 01:58:47 -0400

    feat: update benchmarking scenarios

--- benchmark-tools/go-wrk/install.sh
+++ benchmark-tools/go-wrk/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function install {
-    source ~/.bashrc
+    source ${HOME}/.profile
     sudo apt-get update && sudo apt-get install -y smem jq
     rm -rf /tmp/go-wrk && git clone https://github.com/tsliwowicz/go-wrk.git /tmp/go-wrk && cd /tmp/go-wrk && go build && sudo mv go-wrk /usr/local/bin/
 }
